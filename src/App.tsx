8eae8b2c0f0cca9ac15d8aa51910f53cf829c3cb
Anchitha <anchithasathyan@adhoctechnologies.org>
2023-07-31 17:21:52 +0530

    search box

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import DataGrid from "./Component/DataGrid";
 import TabListView from "./Component/TabListView";
 import SidePopup from "./Component/SidePopup";
 import Header from "./Component/Header";
+import SearchBoxFeild from "./Component/SearchBox";
 
 function App() {
   const [click, setClick] = useState(false);
@@ -14,6 +15,7 @@ function App() {
         <SidePopup />
         <div className="container">
           <TabListView />
+          <SearchBoxFeild />
           <DataGrid />
         </div>
       </div>
